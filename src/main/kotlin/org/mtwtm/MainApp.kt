083c1d7e621539899e48452679ca8f201fb5b0c0
Teng-pao Yu <git@mtwtm.org>
2021-05-04 23:39:55 +0800

    feat: Create Create stage

--- src/main/kotlin/org/mtwtm/MainApp.kt
+++ src/main/kotlin/org/mtwtm/MainApp.kt
@@ -6,8 +6,9 @@ import javafx.geometry.Insets
 import javafx.scene.Scene
 import javafx.scene.control.Alert
 import javafx.scene.control.Button
+import javafx.scene.control.Label
 import javafx.scene.control.TextField
-import javafx.scene.layout.Pane
+import javafx.scene.layout.HBox
 import javafx.scene.layout.Region
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
@@ -59,10 +60,51 @@ class MainApp : Application() {
     }
 
     private fun startCreate(stage: Stage) {
+        val cardLabel = Label(strings.getString(FIELD_CARD))
+        val cardIdField = TextField()
+        val cardHBox = createEvenHBox(cardLabel, cardIdField, INSET_20)
+        val nameLabel = Label(strings.getString(FIELD_NAME))
+        val nameField = TextField()
+        val nameHBox = createEvenHBox(nameLabel, nameField, INSET_20)
+        val phoneLabel = Label(strings.getString(FIELD_PHONE))
+        val phoneField = TextField()
+        val phoneHBox = createEvenHBox(phoneLabel, phoneField, INSET_20)
+        val otherLabel = Label(strings.getString(FIELD_OTHER))
+        val otherField = TextField()
+        val otherHBox = createEvenHBox(otherLabel, otherField, INSET_20)
+        val button = Button(strings.getString(CREATE))
+        val vBox = VBox()
+        vBox.children.addAll(cardHBox, nameHBox, phoneHBox, otherHBox, button)
         stage.title = strings.getString(CREATE)
-        stage.scene = Scene(Pane(), APP_WIDTH, APP_HEIGHT)
+        stage.scene = Scene(vBox, APP_WIDTH, APP_HEIGHT)
+        button.onMouseClicked = EventHandler {
+            if (checkInputNonEmpty(cardIdField, nameField, phoneField)) {
+                showAlert(stage, strings.getString(EMPTY_FIELD), Alert.AlertType.WARNING)
+            } else {
+                insertUser(stage,
+                    User(
+                        cardIdField.text,
+                        nameField.text,
+                        phoneField.text,
+                        otherField.text,
+                        System.currentTimeMillis()
+                    )
+                )
+            }
+        }
+    }
+
+    private fun checkInputNonEmpty(
+        cardIdField: TextField,
+        nameField: TextField,
+        phoneField: TextField
+    ): Boolean {
+        return cardIdField.text.isEmpty() || nameField.text.isEmpty() || phoneField.text.isEmpty()
+    }
+
+    private fun insertUser(stage: Stage, user: User) {
         try {
-            repository.insert(User("", "", "", "", System.currentTimeMillis()))
+            repository.insert(user)
         } catch (ex: IOException) {
             showAlert(stage, strings.getString(CREATE_FAILURE), Alert.AlertType.ERROR)
             stage.close()
@@ -96,6 +138,23 @@ class MainApp : Application() {
         return vBox
     }
 
+    private fun createEvenHBox(regionA: Region, regionB: Region, margin: Insets?): HBox {
+        val hBox = HBox()
+
+        fun setProperties(region: Region) {
+            margin?.let {
+                VBox.setMargin(region, margin)
+            }
+            region.prefHeightProperty().bind(hBox.heightProperty())
+            region.prefWidthProperty().bind(hBox.widthProperty().divide(2))
+        }
+
+        hBox.children.addAll(regionA, regionB)
+        setProperties(regionA)
+        setProperties(regionB)
+        return hBox
+    }
+
     companion object {
         const val APP_WIDTH = 640.0
         const val APP_HEIGHT = 320.0
@@ -105,7 +164,13 @@ class MainApp : Application() {
         const val NOT_FOUND = "org.mtwtm.membersDirectory.notFound"
         const val CREATE_SUCCESS = "org.mtwtm.membersDirectory.createSuccess"
         const val CREATE_FAILURE = "org.mtwtm.membersDirectory.createFailure"
+        const val FIELD_CARD = "org.mtwtm.membersDirectory.field.card"
+        const val FIELD_NAME = "org.mtwtm.membersDirectory.field.name"
+        const val FIELD_PHONE = "org.mtwtm.membersDirectory.field.phone"
+        const val FIELD_OTHER = "org.mtwtm.membersDirectory.field.other"
+        const val EMPTY_FIELD = "org.mtwtm.membersDirectory.emptyField"
         val INSET_50 = Insets(50.0, 50.0, 50.0, 50.0)
+        val INSET_20 = Insets(20.0, 20.0, 20.0, 20.0)
         const val DB = "membersDB"
     }
 }
