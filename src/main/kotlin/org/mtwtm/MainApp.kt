6f6cffa74ed378e6ac32cb34d3685480aeff65f1
Teng-pao Yu <git@mtwtm.org>
2021-05-04 22:24:51 +0800

    feat: Create Search stage

--- src/main/kotlin/org/mtwtm/MainApp.kt
+++ src/main/kotlin/org/mtwtm/MainApp.kt
@@ -4,14 +4,20 @@ import javafx.application.Application
 import javafx.event.EventHandler
 import javafx.geometry.Insets
 import javafx.scene.Scene
+import javafx.scene.control.Alert
 import javafx.scene.control.Button
+import javafx.scene.control.TextField
 import javafx.scene.layout.Pane
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
+import org.mtwtm.data.User
+import org.mtwtm.repository.UserRepository
+import java.io.IOException
 import java.util.*
 
 class MainApp : Application() {
     private val strings: ResourceBundle = ResourceBundle.getBundle("strings")
+    private val repository: UserRepository = UserRepository(DB)
 
     override fun start(stage: Stage) {
         startMain(stage)
@@ -36,26 +42,56 @@ class MainApp : Application() {
         navigateCreate.prefWidthProperty().bind(vBox.widthProperty())
         navigateSearch.prefHeightProperty().bind(vBox.heightProperty().divide(2))
         navigateSearch.prefWidthProperty().bind(vBox.widthProperty())
-        stage.scene = Scene(vBox, WIDTH, HEIGHT)
+        stage.scene = Scene(vBox, APP_WIDTH, APP_HEIGHT)
         stage.show()
     }
 
     private fun startSearch(stage: Stage) {
         stage.title = strings.getString(SEARCH)
-        stage.scene = Scene(Pane(), WIDTH, HEIGHT)
+        val searchContent = TextField()
+        val searchButton = Button()
+        searchButton.text = strings.getString(SEARCH)
+        val vBox = VBox()
+        vBox.children.addAll(searchContent, searchButton)
+        VBox.setMargin(searchContent, INSET_50)
+        VBox.setMargin(searchButton, INSET_50)
+        searchContent.prefHeightProperty().bind(vBox.heightProperty().divide(2))
+        searchContent.prefWidthProperty().bind(vBox.widthProperty())
+        searchButton.prefHeightProperty().bind(vBox.heightProperty().divide(2))
+        searchButton.prefWidthProperty().bind(vBox.widthProperty())
+        searchButton.onMouseClicked = EventHandler {
+            val user = repository.search(searchContent.text)
+            if (user == null) {
+                showAlert(stage, strings.getString(NOT_FOUND), Alert.AlertType.WARNING)
+            } else {
+                showAlert(stage, user.toShortString(), Alert.AlertType.INFORMATION)
+            }
+            searchContent.clear()
+            searchContent.requestFocus()
+        }
+        stage.scene = Scene(vBox, APP_WIDTH, APP_HEIGHT)
     }
 
     private fun startCreate(stage: Stage) {
         stage.title = strings.getString(CREATE)
-        stage.scene = Scene(Pane(), WIDTH, HEIGHT)
+        stage.scene = Scene(Pane(), APP_WIDTH, APP_HEIGHT)
+    }
+
+    private fun showAlert(owner: Stage, title: String, type: Alert.AlertType) {
+        val alert = Alert(type)
+        alert.initOwner(owner)
+        alert.contentText = title
+        alert.showAndWait()
     }
 
     companion object {
-        const val WIDTH = 1280.0
-        const val HEIGHT = 640.0
+        const val APP_WIDTH = 640.0
+        const val APP_HEIGHT = 320.0
         const val TITLE = "org.mtwtm.membersDirectory.title"
         const val SEARCH = "org.mtwtm.membersDirectory.mode.search"
         const val CREATE = "org.mtwtm.membersDirectory.mode.create"
+        const val NOT_FOUND = "org.mtwtm.membersDirectory.notFound"
         val INSET_50 = Insets(50.0, 50.0, 50.0, 50.0)
+        const val DB = "membersDB"
     }
 }
