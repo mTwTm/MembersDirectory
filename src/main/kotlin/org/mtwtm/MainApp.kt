51a8872104f81dee231f453ff44892f763fc8ec7
Teng-pao Yu <git@mtwtm.org>
2021-05-04 15:06:29 +0800

    feat: Add basic i18n support

--- src/main/kotlin/org/mtwtm/MainApp.kt
+++ src/main/kotlin/org/mtwtm/MainApp.kt
@@ -3,13 +3,17 @@ package org.mtwtm
 import javafx.application.Application
 import javafx.scene.Scene
 import javafx.scene.layout.StackPane
+import javafx.scene.text.Text
 import javafx.stage.Stage
+import java.util.*
 
 class MainApp : Application() {
 
     override fun start(stage: Stage) {
+        val strings = ResourceBundle.getBundle("strings")
         stage.title = "JavaFX and Gradle";
-        stage.scene = Scene(StackPane(), 1280.0, 640.0);
+        val text = Text(strings.getString("org.mtwtm.hello"))
+        stage.scene = Scene(StackPane(text), 1280.0, 640.0);
         stage.show();
     }
 }
